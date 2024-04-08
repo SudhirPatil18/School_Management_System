26f894cfd2bed3adbef5ecc34f530114a4645d56
coder-Rit <001patilritesh@gmail.com>
2024-04-08 10:46:35 +0530

    Router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<HomePage></HomePage>}/>
+        <Route path="/login" element={<Login></Login>}/>
+        <Route path="/register" element={<Register></Register>}/>
+      </Routes>
+    </Router>
   );
 }
 
