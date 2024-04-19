671897be8a9b47e5de9032bdcbd6262d80a7a24d
Your Name <you@example.com>
2024-04-19 15:28:28 +0530

    Noticepage.jxsand addnotice.jsx page done

---
+++ frontend/src/Components/macro-Components/AddNewNotice.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './macro.css'
+const AddNewNotice = ({ setOpenModel }) => {
+    return (
+        <>
+            <div className='main1'>
+                <div className='add'>
+
+                    <h2>Notice Form</h2>
+                    <div className="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Title</label>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="formFile" class="form-label">Share Notice File, image</label>
+                        <input class="form-control" type="file" id="formFile" />
+                    </div>
+                    <div class="checkbox-dropdown">
+                        <button class="form-select">SELECT CLASS</button>
+                        <div class="checkbox-dropdown-content">
+                            <label><input type="checkbox" value="1"/> CLASS 1</label>
+                            <label><input type="checkbox" value="2"/> CLASS 2</label>
+                            <label><input type="checkbox" value="3"/> CLASS 3</label>
+                            
+                        </div>
+                    </div>
+
+                    <br />
+
+                    <div class="checkbox-dropdown">
+                        <button class="form-select">SELECT DIVISION</button>
+                        <div class="checkbox-dropdown-content">
+                            <label><input type="checkbox" value="1"/> DIVISION A</label>
+                            <label><input type="checkbox" value="2"/> DIVISION B</label>
+                            <label><input type="checkbox" value="3"/> DIVISION C</label>
+                           
+                        </div>
+                    </div>
+                    <div id='btn' className='flex-center-center btn-collection'>
+             <button type="button" class="btn btn-success" >ADD</button>
+             <button type="button" class="btn btn-danger" onClick={() => {setOpenModel(false)}} >CANCLE</button>
+         </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AddNewNotice;
--- frontend/src/Pages/admin/d_NoticePage.jsx
+++ frontend/src/Pages/admin/d_NoticePage.jsx
@@ -1,11 +1,60 @@
 import React from 'react'
 import PageBody from "../../Components/macro-Components/PageBody";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+import { useState } from 'react';
+import AddNewNotice from '../../Components/macro-Components/AddNewNotice';
 
 const Notice = () => {
+  const [openModal, setOpenModel] = useState(false);
   return (
-    <div>Notice</div>
+    <><nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <br />
+        <button type="button" class="btn btn-warning" onClick={() => { setOpenModel(true) }}>Add New Notice</button>
+      </div>
+    </nav>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col">File</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>
+        <input type="file" accept=".pdf,.png" name="fileUpload"/>
+      </td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+      {
+        openModal === true ? <AddNewNotice setOpenModel={setOpenModel} /> : null
+      }
+
+    </>
   )
 }
+
+
 
 
 const NoticePage = () => {
